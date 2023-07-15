bc12cb5c4f77d99d5a6dae89d356e9bc9bff6106
Amrik Singh <amriksingh@google.com>
2023-07-15 15:06:16 +0300

    simple vm exaple

---
+++ using-module/main.tf
@@ -0,0 +1,28 @@
+data "google_compute_subnetwork" "subnet" {
+    project = "amriksingh-testing"
+  name   = "default"
+  region = "europe-west1"
+}
+
+module "simple-vm-example" {
+  source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric/modules/compute-vm"
+  project_id = "amriksingh-testing"
+  zone       = "europe-west1-b"
+  name       = "test"
+  instance_type = "n2-standard-2"
+  network_interfaces = [{
+    network    = "default"
+    subnetwork = data.google_compute_subnetwork.subnet.self_link
+    nat        = true #Enable public ip
+  }]
+  service_account_create = true
+  boot_disk = {
+    initialize_params = {
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2304-amd64"
+    }
+  }
+
+  metadata = {
+    startup-script = file("startup-script.tpl")
+  }
+}
